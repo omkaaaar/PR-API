5adabbef0162e9a35842cfb66cbfdf7fcddeb04b
omkaaaar <75777792+omkaaaar@users.noreply.github.com>
2025-08-11 21:00:00 +0530

    feat: enhance authentication with session management in sign-up and implement sign-in functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,10 +2,13 @@ import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 import config from "../configs/configs.js";
 import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
 
 const { JWT_SECRET } = config;
 
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
   try {
     const { name, email, password } = req.body;
 
@@ -36,7 +39,43 @@ export const signUp = async (req, res) => {
         userId: newUsers._id,
       },
     });
+
+    await session.commitTransaction();
   } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
     res.status(400).send("Bad request: " + error.message);
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      throw new Error("Incorrect credentials");
+    }
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({
+      success: true,
+      message: "User logged in successfully",
+      data: {
+        token,
+        user,
+      },
+    });
+  } catch (error) {
+    // res.status(400).send("Bad request: " + error.message);
+    next(error);
   }
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import { signUp } from "../controllers/auth.controller.js";
+import { signIn, signUp } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
 authRouter.post("/sign-up", signUp);
-authRouter.post("/sign-in", (req, res) => {
-  res.send({ title: "This is Sign in" });
-});
+authRouter.post("/sign-in", signIn);
 authRouter.post("/sign-out", (req, res) => {
   res.send({ title: "This is Sign out" });
 });
