7869fbe6c5ab67423cefee354d4b3ce1b5807b17
omkaaaar <75777792+omkaaaar@users.noreply.github.com>
2025-09-05 11:37:56 +0530

    fix: improve unauthorized access response formatting in getSubscriptionDetails refactor: remove unnecessary sign-out response in auth.routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,8 +5,6 @@ const authRouter = Router();
 
 authRouter.post("/sign-up", signUp);
 authRouter.post("/sign-in", signIn);
-authRouter.post("/sign-out", (req, res) => {
-  res.send({ title: "This is Sign out" });
-});
+authRouter.post("/sign-out");
 
 export default authRouter;
